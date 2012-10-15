406f1b1e43e1149dfc47e31b2a8783044e828b70
Panu Leppäniemi <me@panuleppaniemi.com>
2012-10-15 22:53:54 +0300

    Added initial UploadBundle

---
+++ src/Discussione/UploadBundle/DiscussioneUploadBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Discussione\UploadBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class DiscussioneUploadBundle extends Bundle
+{
+	public function build(ContainerBuilder $builder)
+	{
+
+	}
+}
