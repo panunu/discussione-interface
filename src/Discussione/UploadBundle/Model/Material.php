34233e4f81ede383be0f2ba928ac61ef75adebf0
Panu Leppäniemi <me@panuleppaniemi.com>
2012-10-17 10:23:04 +0300

    File Model

---
+++ src/Discussione/UploadBundle/Model/Material.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Discussione\UploadBundle\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Material
+{
+	/**
+	 * @Assert\File(mimeTypes = { "text/csv" })
+	 */
+	public $file;
+}
