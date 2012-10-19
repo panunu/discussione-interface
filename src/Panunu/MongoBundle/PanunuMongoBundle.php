04ca7c374801efaabc67e96289feea0325d4fab1
Panu Leppäniemi <me@panuleppaniemi.com>
2012-10-19 11:17:57 +0300

    Initial bundle

---
+++ src/Panunu/MongoBundle/PanunuMongoBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Panunu\MongoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class PanunuMongoBundle extends Bundle
+{
+    public function build(ContainerBuilder $builder)
+    {
+
+    }
+}
